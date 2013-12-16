c34085f2684b5d014b877813e47d9bd707c0a3a9
John Zdanis <Zdanis.john@gmail.com>
2013-12-16 13:25:29 -0500

    added about and contact controllers/routes

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('sampleApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('sampleApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
